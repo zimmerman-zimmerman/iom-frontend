78a26d25e353fc0fe703cc33ebe65dabe4863a32
taufik <tautaufikfik@siemvaessen.local>
2018-05-09 16:18:32 +0200

    Page is missing header

--- src/scenes/donors/donor/Donor.js
+++ src/scenes/donors/donor/Donor.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Layout, Row, Col } from 'antd';
+import _ from "lodash";
 
 import MainHeader from '../../../components/main/MainHeader';
 import MainFooter from '../../../components/main/MainFooter';
 import DonorBreadcrumbs from "./components/DonorBreadcrumb";
 import BaseFilter from '../components/filters/BaseFilter';
-import _ from "lodash";
 import * as actions from "../../../services/actions";
 import {connect} from "react-redux";
 import Projects from "./components/Projects";
+import './styles/Donor.scss';
+
 
 const { Header, Content, Footer } = Layout;
 
@@ -21,7 +23,7 @@ class Donor extends BaseFilter {
       this.actionRequest(
         _.extend({}, params, {participating_organisation_ref: code.toUpperCase()}),
         'participating_organisation',
-        actions.donorsRequest
+        actions.donorRequest
       );
     } else {
       actions.donorInitial();
@@ -31,8 +33,9 @@ class Donor extends BaseFilter {
   render() {
     const code = _.get(this.props, 'match.params.code');
     const data = _.get(this.props, 'donor.data.results[0]');
+    console.log(data);
     return (
-      <Layout>
+      <Layout class="Donor">
         <Header className="Header">
           <MainHeader/>
         </Header>
@@ -41,7 +44,7 @@ class Donor extends BaseFilter {
         </Content>
         <Content className="Content">
           <Row>
-            <Col span={24}>
+            <Col span={24} className="Title">
               <h2 style={{marginTop: 5, marginBottom: 5}}>{data ? data.participating_organisation : null}</h2>
             </Col>
           </Row>
