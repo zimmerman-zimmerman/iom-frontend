53aff67e6ed4f981eccf72f08cf535f27cbc5d18
Martis <elbartas321@gmail.com>
2018-08-22 15:07:34 +0200

    Finally finished with this, had to redo a lot of stuff on the filters, removed the tags appearing from the dropdown filter, changed the filter/value saving logic. And added filter chips where they should be.

--- src/components/base/filters/AccordionFilter.js
+++ src/components/base/filters/AccordionFilter.js
@@ -4,23 +4,78 @@ import injectSheet from 'react-jss';
 import {injectIntl} from "react-intl";
 import Accordion from 'antd-mobile/es/accordion';
 import { Row, Col } from 'react-flexbox-grid';
+import isEqual from 'lodash/isEqual';
+import './styles/AccordionFilter.scss';
+import BaseFilter from './BaseFilter';
 
-const AccordionFilter = (props) => {
-  const { classes, panels } = props;
-  return (
-    <Row className={classes.accordionFilter}>
-      <Col xs={12}>
-        <Accordion className={classes.accordionFilter}>
-          {panels.map((item, index) =>
-            <Accordion.Panel header={item.headerString} key={index}>
-              {item.component}
-            </Accordion.Panel>
-          )}
-        </Accordion>
-      </Col>
-    </Row>
-  )
-};
+class AccordionFilter  extends BaseFilter{
+    constructor(props) {
+        super(props);
+        this.state = {
+            chips: {},
+        };
+    }
+
+  componentDidUpdate(){
+    if(!isEqual(this.props.rootComponent.state.filters.chips, this.state.chips))
+    {
+      this.setState({
+          chips: this.props.rootComponent.state.filters.chips,
+      });
+    }
+  }
+
+  handleRemoveChip(chips, key, index){
+      let values = [];
+      let names = [];
+      if(key !== 'date' && key !== 'money')
+      {
+          values = chips[key].values;
+          names = chips[key].labels;
+          values.splice(index, 1);
+          names.splice(index, 1);
+      }
+      this.handleChange(values, names, true, key, chips[key].type);
+  }
+
+  render () {
+      const { classes, panels } = this.props;
+      const chips = this.state.chips;
+    return(
+        <Row className={classes.accordionFilter}>
+            <div className='filters-container'>
+                {Object.keys(chips).map(key => {
+                    return (
+                        <div className='chip-container'>
+                          <div className='chip-type'>
+                              {chips[key].type}
+                          </div>
+                          <div className='chip-label-container'>
+                              {chips[key].labels.map((label, index) => {
+                                  return (
+                                      <div className='chip-box'>
+                                          <div className='chip-x' onClick={() => this.handleRemoveChip(chips, key, index)}> x </div>
+                                          <div className='chip-label'> {label} </div>
+                                      </div>
+                                  )})}
+                          </div>
+                      </div>
+                    )
+                })}
+            </div>
+            <Col xs={12}>
+                <Accordion className={classes.accordionFilter}>
+                    {panels.map((item, index) =>
+                        <Accordion.Panel header={item.headerString} key={index}>
+                            {item.component}
+                        </Accordion.Panel>
+                    )}
+                </Accordion>
+            </Col>
+        </Row>
+    )
+  }
+}
 
 AccordionFilter.propTypes = {
   rootComponent: PropsType.object,
--- src/components/base/filters/BaseFilter.js
+++ src/components/base/filters/BaseFilter.js
@@ -17,10 +17,13 @@ class BaseFilter extends Component {
         reporting_organisation_identifier: process.env.REACT_APP_REPORTING_ORGANISATION_IDENTIFIER
       },
       update: false,
-      filters: {values: {}, changed: false},
+      filters: {values: {}, changed: false, chips: {}},
       dataRange: [],
       donorTableSortBy: 'participating_organisation',
     };
+    this.actionRequest = this.actionRequest.bind(this);
+    this.updateComponent = this.updateComponent.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   filter(data) {
@@ -34,7 +37,7 @@ class BaseFilter extends Component {
   }
 
   actionRequest(params, groupBy, request) {
-    const { dispatch } = this.props;
+    const dispatch = this.props.dispatch ? this.props.dispatch : this.props.rootComponent.props.dispatch;
     const lParams = params;
     if (groupBy) {
       lParams.group_by = groupBy;
@@ -42,35 +45,71 @@ class BaseFilter extends Component {
     dispatch(request(lParams))
   }
 
-  handleChange(values) {
-    const { fieldName, rootComponent } = this.props;
+  handleChange(values, names, remove=false, fieldN=undefined, fieldL=undefined) {
+    const { rootComponent } = this.props;
+    const fieldName = fieldN ? fieldN : this.props.fieldName;
+    const fieldLabel = fieldL ? fieldL : this.props.fieldLabel;
     const { filters } = rootComponent.state;
+    let value = values;
+    if(fieldName === 'q' && filters.chips['q'] && !remove)
+    {
+      value = filters.chips['q'].values;
+      value.push(values);
+    }
+
     if (get(filters.values, fieldName)) {
       delete filters.values[fieldName];
+      delete filters.chips[fieldName];
     }
-    if (!isEmpty(values)) {
-      filters.values[fieldName] = Array.isArray(values) ? values.join() : values;
+
+    if(fieldName === 'date' && remove){
+        delete filters.values['start_date_gte'];
+        delete filters.values['end_date_lte'];
+        delete filters.chips[fieldName];
+    }
+
+      if(fieldName === 'money' && remove){
+          delete filters.chips[fieldName];
+          rootComponent.setState({
+              dataRange: [],
+          })
+      }
+
+    if (!isEmpty(value)) {
+      const valuez = Array.isArray(value) ? value.join() : value;
+      filters.chips[fieldName] = {
+          values: valuez.split(','),
+          labels: fieldLabel === 'Key word' ? valuez.split(',') : names,
+          type: fieldLabel,
+      };
+
+      filters.values[fieldName] = valuez;
     }
     filters.values['page'] = 1;
     filters.changed = true;
-    this.setState({filters: filters});
+    rootComponent.setState({filters: filters});
+    this.updateComponent(filters);
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const rootComponent = get(this.props, 'rootComponent');
-    if (rootComponent) {
-      const { filters } = rootComponent.state;
-      const { groupBy, filterRequest, secondFilterRequest } = rootComponent.props;
-      if (filters.changed) {
-        const { params } = rootComponent.state;
-        this.actionRequest(extend({}, params, filters.values), groupBy, filterRequest);
-        if (secondFilterRequest) {
-          this.actionRequest(extend({}, params, filters.values), 'participating_organisation', secondFilterRequest);
-        }
-        filters.changed = false;
-        rootComponent.setState({filters: filters})
+  componentDidUpdate() {
+    this.updateComponent()
+  }
+
+  updateComponent(filterz=undefined){
+      const rootComponent = get(this.props, 'rootComponent');
+      if (rootComponent) {
+          const filters = filterz ? filterz : rootComponent.state.filters;
+          const { groupBy, filterRequest, secondFilterRequest } = rootComponent.props;
+          if (filters.changed) {
+              const { params } = rootComponent.state;
+              this.actionRequest(extend({}, params, filters.values), groupBy, filterRequest);
+              if (secondFilterRequest) {
+                  this.actionRequest(extend({}, params, filters.values), 'participating_organisation', secondFilterRequest);
+              }
+              filters.changed = false;
+              rootComponent.setState({filters: filters})
+          }
       }
-    }
   }
 }
 
