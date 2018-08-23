c06f1be93248c92d40b2f2f5b8bb1e7d7d6734ef
Martis <elbartas321@gmail.com>
2018-08-23 17:17:45 +0200

    Ezi typo fix.

--- src/scenes/donors/components/DonorsTable.js
+++ src/scenes/donors/components/DonorsTable.js
@@ -63,7 +63,7 @@ class DonorsTable extends BaseFilter {
     }, {
       title: <span style={tableHeader}>{intl.formatMessage({
         id: 'donors.table.donors.header.projects.count',
-        defaultMessage: 'Implementation projects'
+        defaultMessage: 'Number of projects',
       })}</span>,
       dataIndex: 'activity_count',
       key: 'activity_count',
