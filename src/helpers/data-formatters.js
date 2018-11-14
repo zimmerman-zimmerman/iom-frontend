f19ab43f2a585e157fe5018981bbd0c0b8c24a91
Stefanos <stefanos@zimmermanzimmerman.nl>
2018-11-14 17:01:48 +0200

    Countries table sorting done in front-end

---
+++ src/helpers/data-formatters.js
@@ -0,0 +1,14 @@
+import map from 'lodash/map';
+import get  from 'lodash/get';
+
+export function countriesFormatter(data) {
+    return map(data, (c) => {
+        return {
+            code: get(c, 'recipient_country.code', ''),
+            title: get(c, 'recipient_country.name', ''),
+            budget: get(c, 'value', 0),
+            region: get(c, 'recipient_country.region.name', ''),
+            activity_count: get(c, 'activity_count', 0)
+        };
+    });
+} 
