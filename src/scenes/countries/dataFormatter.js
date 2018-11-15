72a9370f5b672c7103d5d8a6e82c5a0cda8bcd42
Stefanos <stefanos@zimmermanzimmerman.nl>
2018-11-15 10:58:23 +0200

    Adjust region header to be able to sort

--- src/scenes/countries/dataFormatter.js
+++ src/scenes/countries/dataFormatter.js
@@ -1,13 +1,13 @@
 import map from 'lodash/map';
 import get  from 'lodash/get';
 
-export function countriesFormatter(data) {
+export function countriesFormatter(data, m49Region, countryM49Mapping) {
     return map(data, (c) => {
         return {
+            region: get(m49Region, get(countryM49Mapping, get(c, 'recipient_country.code', '').toLowerCase())),
             code: get(c, 'recipient_country.code', ''),
             title: get(c, 'recipient_country.name', ''),
             budget: get(c, 'value', 0),
-            region: get(c, 'recipient_country.region.name', ''),
             activity_count: get(c, 'activity_count', 0)
         };
     });
