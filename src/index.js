9780e52a236ea1fe4efeb0c91fd228d241552810
Stefanos <stefanos@zimmermanzimmerman.nl>
2020-06-04 12:17:50 +0300

    chore: tweak build procedure to bust browser cache

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
+import * as serviceWorker from "./serviceWorker";
 
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { routerMiddleware } from "react-router-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import createHistory from "history/createBrowserHistory";
+import { addLocaleData } from "react-intl";
+import createSagaMiddleware from "redux-saga";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { routerMiddleware } from 'react-router-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import createHistory from 'history/createBrowserHistory';
-import { addLocaleData } from 'react-intl';
-import createSagaMiddleware from 'redux-saga';
+import reducers from "./services/reducers";
+import genericRed from "./services/reducers/generic";
+import sagas from "./services/sagas";
+import "./index.css";
+import App from "./App";
 
-import reducers from './services/reducers';
-import genericRed from './services/reducers/generic';
-import sagas from './services/sagas';
-import './index.css';
-import App from './App';
-
-import en from 'react-intl/locale-data/en';
+import en from "react-intl/locale-data/en";
 
 const sagaMiddleware = createSagaMiddleware();
 const history = createHistory();
 
 const store = createStore(
-    combineReducers({
-        ...reducers,
-        ...genericRed,
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(sagaMiddleware, routerMiddleware(history)),
+  combineReducers({
+    ...reducers,
+    ...genericRed,
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleware, routerMiddleware(history))
 );
 
 addLocaleData([...en]);
 sagaMiddleware.run(sagas);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
